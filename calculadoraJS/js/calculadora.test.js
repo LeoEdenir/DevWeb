cb7907ddf96843627c65acf817b13b7ba43e82cc
Leonardo Graciano <lgraciano@raioss.net>
2022-03-30 11:42:27 -0300

    ajustar pastas

--- calculadoraJS/js/calculadora.test.js
+++
@@ -1,9 +0,0 @@
-const soma = require('./soma')
-
-it('Soma de dois positivos', ()=>{
-    expect(soma(2,4)).toBe(6)
-})
-
-it('Soma de positivo e negativo', ()=>{
-    expect(soma(-2,4)).toBe(2)
-})
